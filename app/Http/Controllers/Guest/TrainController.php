53610451c33c81fe3f98059e1f26c6a8c92a2b36
Luigidefilippo <defilippoluigi65@gmail.com>
2023-05-28 11:35:33 +0200

    esercizio completo ma da fixare la visualizzazione dei train

--- app/Http/Controllers/Guest/TrainController.php
+++ app/Http/Controllers/Guest/TrainController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class TrainController extends Controller
 {
     public function index(){
-        $trains = Train::all();
+        $trains= Train::where('departure_time', '>=', now())->get();
         return view('Trains.index');
     }
 }
--- resources/views/trains/index.blade.php
+++ resources/views/trains/index.blade.php
@@ -3,7 +3,22 @@
 @section('content')
 <div class="container">
     <h1>Trains</h1>
-    
+    <ul>
+        @foreach ($trains as $train)
+        <li>
+            <p>
+                <strong>Partenza da </strong>{{ $train->departure_station}}
+            </p>
+            
+            <p>
+                <strong>Partenza da </strong>{{ $train->arrival_station}}
+            </p>
+
+        </li>
+            
+        @endforeach
+    </ul>
+
 </div>
     
 @endsection
